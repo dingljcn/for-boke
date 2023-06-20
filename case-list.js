7106910fa2a48bd21b72c4fece045230e05d3ef5
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-06-20 15:36:32 +0800

    Update case-list.js

--- case-list.js
+++ case-list.js
@@ -1,12 +1,18 @@
-function run(config) {
+function run(config = null) {
+    if (config == null) {
+        alert('未传入配置项, 脚本终止执行');
+        return;
+    }
     // 引入通用脚本
     let utilScript = document.createElement('script');
     utilScript.type = 'text/javascript';
     utilScript.src = 'https://dingljcn.github.io/for-boke/utils.js?' + Math.random();
     // 脚本加载完成之后读取开始读配置, 启动脚本
     utilScript.onload = () => {
-        console.log('传入的配置项: ')
-        console.log(config);
+        if (isMatch(config)) {
+            console.log('传入的配置项: ')
+            console.log(config);
+        }
     };
     document.head.appendChild(utilScript);
 }
