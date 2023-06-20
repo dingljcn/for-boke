587ab3ae1c12d17eee3773843687da4c1f41887b
dingljcn <dljmt@qq.com>
2023-06-20 16:14:31 +0800

    读取模块

--- beta/case-list.js
+++ beta/case-list.js
@@ -32,14 +32,34 @@ function run(config = null) {
     utilScript.type = 'text/javascript';
     utilScript.src = 'https://dingljcn.github.io/for-boke/beta/utils.js?' + Math.random();
     // 脚本加载完成之后读取开始读配置, 启动脚本
-    utilScript.onload = () => {
+    utilScript.onload = async function() {
         if (isMatch(config)) {
             console.log('传入的配置项: ')
             console.log(config);
+            await getAllModule();
         } else {
             console.log('当前网址不符合以下匹配规则:');
             console.log(config.matchList);
         }
     };
     document.head.appendChild(utilScript);
+}
+
+/** 读取所有模块 */
+async function getAllModule() {
+    let response = await axios.get(`${ window.location.href }TestCaseServlet?getModule=true`);
+    console.log('module response: ');
+    console.log(response);
+    globalData.modules = response.data.sort((m1, m2) => {
+        let i1 = config.you.preferModule.indexOf(m1);
+        if (i1 == -1) {
+            i1 = 65535;
+        }
+        let i2 = config.you.preferModule.indexOf(m2);
+        if (i2 == -1) {
+            i2 = 65535;
+        }
+        return i1 - i2;
+    });
+    globalData.modules.unshift('UNIT');
 }
