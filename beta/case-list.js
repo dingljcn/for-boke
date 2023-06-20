7bb141b7cfb3f97f5988fbe4e81071cfad5c48bb
dingljcn <dljmt@qq.com>
2023-06-20 16:20:27 +0800

    读取版本

--- beta/case-list.js
+++ beta/case-list.js
@@ -22,6 +22,12 @@ class Case {
     }
 }
 
+const globalData = {
+    modules: [],
+    history: [],
+    versions: [],
+}
+
 function run(config = null) {
     if (config == null) {
         alert('未传入配置项, 脚本终止执行');
@@ -36,7 +42,8 @@ function run(config = null) {
         if (isMatch(config)) {
             console.log('传入的配置项: ')
             console.log(config);
-            await getAllModule();
+            await getAllModule(config);
+            await readVersions(config);
         } else {
             console.log('当前网址不符合以下匹配规则:');
             console.log(config.matchList);
@@ -46,20 +53,24 @@ function run(config = null) {
 }
 
 /** 读取所有模块 */
-async function getAllModule() {
+async function getAllModule(config) {
     let response = await axios.get(`${ window.location.href }TestCaseServlet?getModule=true`);
-    console.log('module response: ');
+    console.log('所有模块: ');
     console.log(response);
-    globalData.modules = response.data.sort((m1, m2) => {
-        let i1 = config.you.preferModule.indexOf(m1);
-        if (i1 == -1) {
-            i1 = 65535;
-        }
-        let i2 = config.you.preferModule.indexOf(m2);
-        if (i2 == -1) {
-            i2 = 65535;
-        }
+    globalData.modules = response.data.sort((m1, m2) => { //把最关注的模块排在前面
+        let i1 = config.preferModule.indexOf(m1);
+        let i2 = config.preferModule.indexOf(m2);
+        i1 = i1 == -1 ? 65535 : i1;
+        i2 = i2 == -1 ? 65535 : i2;
         return i1 - i2;
     });
-    globalData.modules.unshift('UNIT');
+    globalData.modules.unshift('UNIT'); // 单元测试始终在最前面
+}
+
+async function readVersions(config) {
+    let response = await axios.get(`${ window.location.href }EVersionServlet`);
+    console.log('所有版本: ');
+    console.log(response);
+    globalData.history = response.data || [];
+    globalData.versions = globalData.history.map(i => i.erpVersion);
 }
