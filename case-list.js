5a4385decbd18ddedb95776a91a3df181817f416
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-06-20 16:02:01 +0800

    Update case-list.js

--- case-list.js
+++ case-list.js
@@ -1,3 +1,27 @@
+/** 用例类型 */
+class Case {
+    status; module; caseName; // 三个查询条件
+    clevel; row; totalRow; dbType; timeCost; ticketID;
+    constructor(origin) {
+        this.status = origin.result;
+        if (!this.status) { //状态不为空, 则把中文转英文
+            if (origin.stats == '执行中') {
+                this.status = 'RUNNING';
+            } else if (origin.stats == '已发送') {
+                this.status = 'WAITTING';
+            }
+        }
+        this.module = origin.module;
+        this.caseName = origin.testcaseName || origin.testCaseName;
+        this.clevel = origin.clevel || origin.level;
+        this.row = origin.currentRow;
+        this.totalRow = origin.totalRow;
+        this.dbType = origin.dbType;
+        this.timeCost = origin.timeCost || '-';
+        this.ticketID = origin.log || origin.ticketId || 0;
+    }
+}
+
 function run(config = null) {
     if (config == null) {
         alert('未传入配置项, 脚本终止执行');
