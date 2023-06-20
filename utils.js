40aa2395c8916d068978f5797ea3613013298a0e
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-06-20 15:37:51 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,14 +1,15 @@
 /** 判断当前网址是否启用脚本 */
 function isMatch(config) {
-    // 如果存在要匹配的网址, 则匹配, 匹配成功才进入
-    if (config.matchList) {
+    if (config.matchList) { // 如果存在要匹配的网址, 则匹配, 匹配成功才进入
         for (let stratege of config.matchList) {
             if (stratege.test(window.location.href)) {
                 return true;
             }
         }
+    } else { // 如果没有要匹配的网址, 默认是允许执行脚本
+        return true;
     }
-    return false;
+    return false; // 到这里说明存在要匹配的网址, 但匹配失败, 返回 false
 }
 
 /** 对象转 json 字符串 */
