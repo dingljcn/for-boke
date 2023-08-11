ac77ed6a791882184828632bd2618d2046fd283d
dingljcn <dljmt@qq.com>
2023-08-11 23:29:04 +0800

    '鍥剧墖鏄剧ず'

--- beta/utils.js
+++ beta/utils.js
@@ -277,15 +277,14 @@ function toSecond(number = 0, originUnit = 'hour') {
  * @param {string} path 完整地址或相对地址
  * @param {Function} callback 回调函数
  */
-async function get(path = '', callback = res => {}) {
+function get(path = '', sync = false, callback = res => {}) {
     const http = new XMLHttpRequest();
-    http.open('GET', path);
+    http.open('GET', path, sync);
     http.send();
-    http.onreadystatechange = res => {
-        if (http.readyState == 4 && http.status == 200) {
-            callback(http.responseText);
-        }
+    if (http.readyState == 4 && http.status == 200) {
+        callback(http.responseText);
     }
+    return http.responseText;
 }
 
 function log(obj) {
