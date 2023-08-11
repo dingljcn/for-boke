edaa23adfced695005b0011eec97095fc8f40137
dingljcn <dljmt@qq.com>
2023-08-11 23:18:35 +0800

    '鍥剧墖鏄剧ず'

--- beta/tmp.js
+++ beta/tmp.js
@@ -1,47 +1,3 @@
-const context_001 = {
-    imageList: [],
-    focus: 'step', // line, step, his, star
-    layout: ['line', 'step', 'his', 'star'],
-};
-
-function onload_001(callback) {
-    // 引入通用脚本
-    let utilScript = document.createElement('script');
-    utilScript.type = 'text/javascript';
-    utilScript.src = 'https://dingljcn.github.io/for-boke/beta/utils.js?' + Math.random();
-    // 成功
-    utilScript.onload = async function() {
-        localStorage.setItem(`dinglj-script-001`, utilScript.src); // 缓存本次成功 load 的 url
-        callback();
-    }
-    // 失败
-    utilScript.onerror = () => {
-        let lastURL = localStorage.getItem(`dinglj-script-001`);
-        console.error(`${ utilScript.src } 拉取失败, 拉取上次成功的地址 ${ lastURL }`);
-        utilScript.remove();
-        appendScript(callback);
-    }
-    document.head.appendChild(utilScript);
-}
-
-function run_001(config) {
-    // 检验配置
-    context_001.config = config;
-    logln('传入的配置: ', context_001.config);
-    if (!isMatch(context_001.config)) {
-        console.error('不符合以下地址匹配规则');
-        console.error(context_001.config.matchList);
-        return;
-    }
-    // 正式开始独立的逻辑
-    exec_001();
-}
-
-function exec_001() {
-    initLayout_001();
-}
-
-
 const css_const = {
     title: {
         background: '#321b33',
@@ -182,15 +138,15 @@ const css_001 = `body {
     color: white;
     border-radius: 5px;
     font-weight: bolder;
-}`;
-
+}
+`;
+newElement('style', {
+    parentNode: document.head
+}, {
+    innerText: css_001
+}, []);
 
 function initLayout_001() {
-    newElement('style', {
-        parentNode: document.head
-    }, {
-        innerText: css_001
-    }, []);
     document.body.innerHTML = `<div id="dinglj-all-container">
         <div id="dinglj-all-title">
             <div id="dinglj-web-name">用例截图查看工具</div>
@@ -332,3 +288,5 @@ function initLayout_001() {
         </div>
     </div>`;
 }
+
+initLayout_001();
