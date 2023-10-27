ef25aa87c233694a57987932f8ab9fa8dc05be78
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-10-27 19:37:34 +0800

    Create tool.js

---
+++ beta/test/tool.js
@@ -0,0 +1,11 @@
+const context_004 = {
+    config: {},
+    rt: {
+        tickets: [],
+    },
+    fields: {
+        origin: ['id', 'priority', 'owner', 'create_time', 'modify_time', 'from_create', 'to_end', 'component', 'summary', 'status'], /* 变更报表原字段顺序 */
+        display: ['id', 'summary', 'status', 'priority', 'component', 'owner', 'create_time', 'modify_time', 'from_create', 'to_end'], /* 要显示的字段顺序 */
+        zhCN: ['变更号', '概述', '状态', '优先级', '模块', '属主', '创建时间', '修改时间', '已创建天数', '截至上次修改天数'], /* 要显示的字段顺序名称 */
+    }
+}
