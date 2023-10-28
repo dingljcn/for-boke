2976ff36a5f779918ebb3d86ed9d022a92e3d1a3
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-10-28 20:09:37 +0800

    Update index.js

--- beta/test/index.js
+++ beta/test/index.js
@@ -165,9 +165,7 @@ function generateTableData_004(tableKey = '', list) {
         </div>`;
         for (let i = 1; i < context_004.fields.display.length; i++) {
             let key = context_004.fields.display[i];
-            lineHTML += `<div class="dinglj-cell dinglj-column-${ key }" ${
-                key == 'summary' ?  `onmouseover="onMouseIn(this,'${ ticketID }')" onmouseout="getById('menu-for-${ ticketID }').remove()"` : ''
-            }>${ line[key] }</div>`;
+            lineHTML += `<div class="dinglj-cell dinglj-column-${ key }" ${ key == 'summary' ?  `onmouseover="onMouseIn(this,'${ ticketID }')"` : '' }>${ line[key] }</div>`;
         }
         html.push(`<div class="dinglj-tr dinglj-data">${ lineHTML }</div>`);
     }
