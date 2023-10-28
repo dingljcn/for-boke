65f3d5a732d7de599c85e9e2ac8f139b7820087e
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-10-28 15:47:02 +0800

    Update tool.js

--- beta/test/tool.js
+++ beta/test/tool.js
@@ -9,7 +9,7 @@ const context_004 = {
     fields: {
         origin: ['id', 'priority', 'owner', 'create_time', 'modify_time', 'from_create', 'to_end', 'component', 'summary', 'status'], /* 变更报表原字段顺序 */
         display: ['id', 'summary', 'status', 'priority', 'component', 'owner', 'create_time', 'modify_time', 'from_create', 'to_end'], /* 要显示的字段顺序 */
-        zhCN: ['变更号', '概述', '状态', '优先级', '模块', '属主', '创建时间', '修改时间', '已创建天数', '截至上次修改天数'], /* 要显示的字段顺序名称 */
+        zhCN: ['变更号', '概述', '状态', '优先级', '模块', '属主', '创建时间', '修改时间', '已创建天数', '上次处理天数'], /* 要显示的字段顺序名称 */
     }
 }
 
