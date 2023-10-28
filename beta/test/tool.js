80f0422f9efc4d2b9aeff22a1d5c41c97e5202ce
dingljcn <137146489+dingljcn@users.noreply.github.com>
2023-10-28 19:38:10 +0800

    Update tool.js

--- beta/test/tool.js
+++ beta/test/tool.js
@@ -62,7 +62,7 @@ function onNavChange_004(name) {
     getById(`mode-${ name }`).classList.add('active-nav');
     let key = decodeUID(name);
     let tickets = context_004.rt.filteredData[key];
-    getById('ticket-list-view').innerHTML = generateTable_004(tickets);
+    getById('ticket-list-view').innerHTML = generateTable_004(name, tickets);
     context_004.fields.display.forEach(key => {
         if (key == 'summary') {
             return;
